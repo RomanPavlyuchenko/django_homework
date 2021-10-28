0ec80e79c889a4d4a411798c3c5b0bd105400bea
Viper24rus <756465536558>
2021-10-28 21:01:40 +0700

    1.2 solved

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,7 @@
+import os
+from csv import DictReader
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +11,24 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
 
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+    context = dict()
+
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data-398-2018-08-30.csv')
+    bus_stations = list()
+    with open(file_path, encoding='utf-8') as file:
+        reader = DictReader(file)
+        for row in reader:
+            bus_station = dict()
+            bus_station['Name'] = row['Name']
+            bus_station['Street'] = row['Street']
+            bus_station['District'] = row['District']
+            bus_stations.append(bus_station)
+
+    paginator = Paginator(bus_stations, 10)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    context['page'] = page
+    context['bus_stations'] = page.object_list
+
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +17,17 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, dish):
+    data = DATA.get(dish)
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': dict(),
+    }
+
+    if data:
+        for ingredient, count in data.items():
+            context['recipe'][ingredient] = count * servings
+    return HttpResponse(render(request, 'calculator/index.html', context))
